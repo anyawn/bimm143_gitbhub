c1ec78ca18c450f5b8654f27a2705ce60f53e549
Ayanna Kerr <akkerr@ucsd.edu>
2024-05-21 11:54:30 -0700

    Add class files

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class 4: R Basics"
+#' author: "Ayanna Kerr"
+#' date: "April 11, 2024"
+#' ---
+#this is a text file
+x <- 1:50
+plot(x,sin(x))
+plot(x,sin(x),typ='l',col="red",lwd = 3)
